ff9ddd84b2b2342b59330f73848a940cfd082ba4
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2022-10-07 23:53:34 +0100

    Email message

---
+++ dist/email/emailTemplate.js
@@ -0,0 +1,3 @@
+"use strict";
+const ;
+//# sourceMappingURL=emailTemplate.js.map
---
+++ src/email/emailVerification.ts
@@ -0,0 +1,30 @@
+export const emailVerification = (token:string):string => {
+    const link = `${process.env.BACKEND_URL}/users/verify/${token}`;
+    let temp = `
+     <div   style="max-width: 700px;
+            margin:auto; 
+            border: 10px solid #ddd; 
+            padding: 50px 20px; 
+            font-size: 110%;">
+        <h2 
+            style="text-align: 
+            center; text-transform: 
+            uppercase;color: teal;">
+            Welcome to Airtime to Cash POD G.
+        </h2>
+        <p>
+            Hi there, Follow the link by clicking on the button to verify your email
+        </p>
+       <a href=${link}
+            style="background: crimson; 
+            text-decoration: none; 
+            color: white;
+            padding: 10px 20px; 
+            margin: 10px 0;
+            display: inline-block;">
+            Click here
+        </a>
+    </div>
+      `;
+  return temp;
+}
